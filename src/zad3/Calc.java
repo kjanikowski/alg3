ff5264fae65af11c0e8af09ce19fd98d963605e8
kriss7676 <krzysi3k96@gmail.com>
2018-12-11 15:01:52 +0100

    init

---
+++ src/zad3/Calc.java
@@ -0,0 +1,23 @@
+package zad3;
+
+public class Calc {
+	
+	double equations[][];
+	double answ[]; 
+	int size;
+	
+	
+	public double[] jacobi() {
+		
+		
+		
+		
+		
+		
+		return answ;
+		
+	}
+	
+	
+
+}
